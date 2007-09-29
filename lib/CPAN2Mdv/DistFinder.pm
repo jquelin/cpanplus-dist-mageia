69d3d34f50bf8e39b0a4dc123c70dfc22d1fef08
Jérôme Quelin <jquelin@gmail.com>
2007-09-29 10:27:42 +0000

    r6302@merlin:  jquelin | 2007-09-28 19:06:10 +0200  new worker: distfinder

---
+++ lib/CPAN2Mdv/DistFinder.pm
@@ -0,0 +1,49 @@
+#
+#
+#
+
+package CPAN2Mdv::DistFinder;
+
+use POE;
+
+
+
+#--
+# constructor
+
+sub spawn {
+    my ($type, $args) = @_;
+
+    my $session = POE::Session->create(
+        args          => [ $args ],
+        inline_states => {
+            '_start'    => \&_onpriv_start,
+        },
+    );
+    return $session->ID;
+}
+
+
+#--
+# private events
+
+sub _onpriv_start {
+    my ($k, $h, $args) = @_[KERNEL, HEAP, ARG0];
+    my $alias = 'distfinder';
+
+    # store config
+    $h->{conf}  = $args;
+    my $pkgfile = $h->{conf}{General}{cache} . '/02packages.details.txt';
+    open my $pkgfh, '<', $pkgfile or die "can't open '$pkgfile': $!";
+    $h->{pkgfh} = $pkgfh;
+
+    $k->alias_set( $alias );
+    $k->post( 'journal', 'ident',      $alias );       # register to journal
+    $k->post( 'main',    'rendezvous', $alias );       # signal main that we're started
+    $k->post( 'journal', 'log', "start complete\n" );  # logging
+}
+
+
+
+1;
+__END__
