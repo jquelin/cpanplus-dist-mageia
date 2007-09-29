84fb675c00c9b2ae6bd1c9a07f4297045a855517
Jérôme Quelin <jquelin@gmail.com>
2007-09-29 10:29:20 +0000

    r6313@merlin:  jquelin | 2007-09-29 10:59:19 +0200  new module: collector

---
+++ lib/CPAN2Mdv/Collector.pm
@@ -0,0 +1,50 @@
+#
+#
+#
+
+package CPAN2Mdv::Collector;
+
+use POE;
+
+
+
+#--
+# constructor
+
+sub spawn {
+    my ($type, $args) = @_;
+
+    my $session = POE::Session->create(
+        args          => [ $args ],
+        inline_states => {
+            '_start'  => \&_onpriv_start,
+        },
+    );
+    return $session->ID;
+}
+
+
+#--
+
+
+#--
+# private events
+
+sub _onpriv_start {
+    my ($k, $h, $args) = @_[KERNEL, HEAP, ARG0];
+    my $alias = 'collector';
+
+    # store config.
+    $h->{conf}  = $args;
+
+    # set alias and finish startup.
+    $k->alias_set( $alias );
+    $k->post( 'journal', 'ident',      $alias );       # register to journal
+    $k->post( 'main',    'rendezvous', $alias );       # signal main that we're started
+    $k->post( 'journal', 'log', "start complete\n" );  # logging
+}
+
+
+
+1;
+__END__
