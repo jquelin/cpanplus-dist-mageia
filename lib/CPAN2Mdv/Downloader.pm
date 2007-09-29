0810e6783887d0e3f0cb919f2663d9a68f350df8
Jérôme Quelin <jquelin@gmail.com>
2007-09-29 10:31:27 +0000

    r6326@merlin:  jquelin | 2007-09-29 12:06:45 +0200  new session Downloader

---
+++ lib/CPAN2Mdv/Downloader.pm
@@ -0,0 +1,56 @@
+#
+#
+#
+
+package CPAN2Mdv::Downloader;
+
+use HTML::TreeBuilder;
+use POE;
+
+
+
+#--
+# constructor
+
+sub spawn {
+    my ($type, $args) = @_;
+
+    my $session = POE::Session->create(
+        args          => [ $args ],
+        inline_states => {
+            '_start'  => \&_onpriv_start,
+            'task'    => \&_onpub_task,
+        },
+    );
+    return $session->ID;
+}
+
+
+#--
+#
+
+sub _onpub_task {
+}
+
+
+#--
+# private events
+
+sub _onpriv_start {
+    my ($k, $h, $args) = @_[KERNEL, HEAP, ARG0];
+    my $alias = 'downloader';
+
+    # store config.
+    $h->{conf}  = $args;
+
+    # set alias and finish startup.
+    $k->alias_set( $alias );
+    $k->post( 'journal', 'ident',      $alias );       # register to journal
+    $k->post( 'main',    'rendezvous', $alias );       # signal main that we're started
+    $k->post( 'journal', 'log', "start complete\n" );  # logging
+}
+
+
+
+1;
+__END__
