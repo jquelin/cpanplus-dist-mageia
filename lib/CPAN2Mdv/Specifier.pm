af82fc13f7a9bc32e3cd4aaf320906e86fb859d5
Jérôme Quelin <jquelin@gmail.com>
2007-09-30 17:58:13 +0000

    r6354@merlin:  jquelin | 2007-09-29 12:29:40 +0200  new module Specifier

---
+++ lib/CPAN2Mdv/Specifier.pm
@@ -0,0 +1,52 @@
+#
+#
+#
+
+package CPAN2Mdv::Specifier;
+
+use strict;
+use warnings;
+
+use POE;
+
+
+
+#--
+# constructor
+
+sub spawn {
+    my ($type, $args) = @_;
+
+    my $session = POE::Session->create(
+        args          => [ $args ],
+        inline_states => {
+            '_start'  => \&_onpriv_start,
+            'task'    => \&_onpub_task,
+        },
+    );
+    return $session->ID;
+}
+
+
+
+#--
+# private events
+
+sub _onpriv_start {
+    my ($k, $h, $args) = @_[KERNEL, HEAP, ARG0];
+    my $alias = 'specifier';
+
+    # store config.
+    $h->{conf}  = $args;
+
+    # set alias and finish startup.
+    $k->alias_set( $alias );
+    $k->post( 'journal', 'ident',      $alias );       # register to journal
+    $k->post( 'main',    'rendezvous', $alias );       # signal main that we're started
+    $k->post( 'journal', 'log', "start complete\n" );  # logging
+}
+
+
+
+1;
+__END__
