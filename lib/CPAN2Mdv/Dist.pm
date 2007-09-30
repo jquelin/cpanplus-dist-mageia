f84353fa06fe5d4eb6505e8d58de2d753aed0a05
Jérôme Quelin <jquelin@gmail.com>
2007-09-30 18:07:39 +0000

    r6422@merlin:  jquelin | 2007-09-30 19:30:06 +0200  new attributes build_requires & srpm

--- lib/CPAN2Mdv/Dist.pm
+++ lib/CPAN2Mdv/Dist.pm
@@ -9,8 +9,8 @@ use warnings;
 
 use base qw[ Class::Accessor::Fast ];
 __PACKAGE__->mk_accessors
-    ( qw[ description extra_files is_prereq name module path pkgname rpm
-        specfile summary url version ] );
+    ( qw[ build_requires description extra_files is_prereq name module path pkgname rpm
+        specfile srpm summary url version ] );
 
 1;
 __END__
