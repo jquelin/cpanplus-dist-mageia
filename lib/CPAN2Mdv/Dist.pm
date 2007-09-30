fd32eede2ad60352bee253c4b84a8fbb8c28ba39
Jérôme Quelin <jquelin@gmail.com>
2007-09-30 17:58:55 +0000

    r6374@merlin:  jquelin | 2007-09-29 12:51:05 +0200  new attirbutes: specfile and pkgname

--- lib/CPAN2Mdv/Dist.pm
+++ lib/CPAN2Mdv/Dist.pm
@@ -9,7 +9,7 @@ use warnings;
 
 use base qw[ Class::Accessor::Fast ];
 __PACKAGE__->mk_accessors
-    ( qw[ name module path url version ] );
+    ( qw[ name module path pkgname specfile url version ] );
 
 1;
 __END__
