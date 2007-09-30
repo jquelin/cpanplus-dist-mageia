08260514b30e5430cc8c2d3b35097af4d2176971
Jérôme Quelin <jquelin@gmail.com>
2007-09-30 18:03:28 +0000

    r6400@merlin:  jquelin | 2007-09-29 16:29:51 +0200  new attribute rpm

--- lib/CPAN2Mdv/Dist.pm
+++ lib/CPAN2Mdv/Dist.pm
@@ -9,7 +9,7 @@ use warnings;
 
 use base qw[ Class::Accessor::Fast ];
 __PACKAGE__->mk_accessors
-    ( qw[ extra_files is_prereq name module path pkgname specfile url version ] );
+    ( qw[ extra_files is_prereq name module path pkgname rpm specfile url version ] );
 
 1;
 __END__
