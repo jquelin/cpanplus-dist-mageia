183c988046979a5d03bb561cc476bf9998a47656
Jérôme Quelin <jquelin@gmail.com>
2009-07-01 17:24:22 +0200

    copyright boilerplate added by dist::zilla::plugin::prepender

--- lib/CPANPLUS/Dist/Mdv.pm
+++ lib/CPANPLUS/Dist/Mdv.pm
@@ -1,11 +1,3 @@
-#
-# This file is part of CPANPLUS::Dist::Mdv.
-# Copyright (c) 2007 Jerome Quelin, all rights reserved.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the same terms as Perl itself.
-#
-
 package CPANPLUS::Dist::Mdv;
 
 use strict;
